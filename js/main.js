cd6d24a686127b13929a65f0b022e38e7312caf9
Daniel McMahon <daniel.m.mve@gmail.com>
2018-06-23 11:36:18 -0500

    previous status is incorrect. Changes were saved in incorrect file. Previous changes corrected and status is current

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 
+ //Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCvPelSDe2zsGCMDOqQufLO2JOBuAXA9yQ",
+    authDomain: "reservation-site-c6de4.firebaseapp.com",
+    databaseURL: "https://reservation-site-c6de4.firebaseio.com",
+    projectId: "reservation-site-c6de4",
+    storageBucket: "reservation-site-c6de4.appspot.com",
+    messagingSenderId: "727532856447"
+  };
+  firebase.initializeApp(config);
 
+//
+var reservationData = {};
 
-
-
-
-
+//Initialize Map
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 40.8054491, lng: -73.9654415},
